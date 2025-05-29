05018f0c58621c0d7f65ef00df945545e38d13da
Mousa Pirzada <mousapir@gmail.com>
2025-05-29 17:51:07 -0400

    Completed Day001 project

--- Day001/main.py
+++ Day001/main.py
@@ -1,1 +1,4 @@
-ECHO is on.
+#Band Name Generator
+city = input("Welcome to the Band Name Generator.\nWhat's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " "+ pet)
